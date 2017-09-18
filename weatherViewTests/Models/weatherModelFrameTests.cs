6d466000ebc9d61c0060c8c92e38ef4bbf7ef71c
Noelcke <noelcket@oregonstate.edu>
2017-09-18 09:49:08 -0700

    Added unit tests

---
+++ weatherViewTests/Models/weatherModelFrameTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Tests
+{
+    [TestClass()]
+    public class weatherModelFrameTests
+    {
+        [TestMethod()]
+        public void setCloudForcastTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void getCloudForcastTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void setWindDirectionTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void getWindDirectionTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void setWindSpeedTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void getWindSpeedTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ weatherViewTests/Models/weatherModelRunTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Tests
+{
+    [TestClass()]
+    public class weatherModelRunTests
+    {
+        [TestMethod()]
+        public void weatherModelRunTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void getAverageMaxTempTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void getAverageMinTempTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void getAverageTempTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
